7bc6e714a9dabb38f7b00eefe7fe1b8003819cd8
Gabriel Costanza <costanzag1999@gmail.com>
2022-10-30 12:15:59 +0100

    env variables

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -6,15 +6,19 @@ import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService
   ) {
-
+    this.defaultLimit = this.configService.get<number>('defaultLimit');
   }
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -30,7 +34,7 @@ export class PokemonService {
 
   findAll(paginationDto: PaginationDto) {
 
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
 
     return this.pokemonModel.find()
       .limit(limit)
@@ -39,8 +43,8 @@ export class PokemonService {
         no: 1
       })
       .select('-__v');
-      //el select esta removiendo la propiedad __v gracias al -
-      //en sort pones la propiedad por la que deseas ordenar y el valor numero indica ascendente o descendente
+    //el select esta removiendo la propiedad __v gracias al -
+    //en sort pones la propiedad por la que deseas ordenar y el valor numero indica ascendente o descendente
   }
 
   async findOne(term: string) {
